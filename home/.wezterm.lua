6277444b28f575d30e719b740d379c3930607992
Péter Szabados <peter.szabados@tresorit.com>
2025-01-21 17:23:27 +0100

    hide_tab_bar_if_only_one_tab

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -11,6 +11,7 @@ config.color_scheme = "Selenized Dark (Gogh)"
 
 config.font = wezterm.font("DejaVu Sans Mono", {})
 config.font_size = 12
+config.hide_tab_bar_if_only_one_tab = true
 
 -- and finally, return the configuration to wezterm
 return config
