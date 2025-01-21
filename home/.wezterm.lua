efc0f73e73d129b1f1660a8c2fd708bf9c470072
Péter Szabados <peter.szabados@tresorit.com>
2025-01-21 17:24:07 +0100

    Remove comments

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -1,17 +1,9 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
 config.color_scheme = "Selenized Dark (Gogh)"
-
 config.font = wezterm.font("DejaVu Sans Mono", {})
 config.font_size = 12
 config.hide_tab_bar_if_only_one_tab = true
 
--- and finally, return the configuration to wezterm
 return config
