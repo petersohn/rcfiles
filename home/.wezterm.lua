97a6783f5c00ea6e2a2ef803ee21f2c44665731e
Péter Szabados <peter.szabados@tresorit.com>
2025-03-21 18:15:10 +0100

    Remove wezterm window padding

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -5,5 +5,11 @@ config.color_scheme = "Selenized Dark (Gogh)"
 config.font = wezterm.font("DejaVu Sans Mono", {})
 config.font_size = 12
 config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
 
 return config
