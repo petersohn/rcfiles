84b34b13d7fe6948e39824db629767180a292899
Péter Szabados <peter.szabados@tresorit.com>
2025-06-11 18:30:37 +0200

    Disable treesitter indenting

--- home/.config/nvim/lua/custom/plugins/treesitter.lua
+++ home/.config/nvim/lua/custom/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  branch = 'master',
   build = ':TSUpdate',
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
@@ -14,7 +15,9 @@ return {
       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
       additional_vim_regex_highlighting = { 'ruby' },
     },
-    indent = { enable = true, disable = { 'ruby' } },
+    indent = {
+      enable = false,
+    },
   },
   -- There are additional nvim-treesitter modules that you can use to interact
   -- with nvim-treesitter. You should go explore a few and see what interests you:
