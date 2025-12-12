8375631c0536e23d95010156fae3938d647cd650
Peter Szabados <kangirigungi@gmail.com>
2025-12-12 19:52:21 +0100

    rust-analyzer settings

--- home/.config/nvim/lua/custom/plugins/lsp.lua
+++ home/.config/nvim/lua/custom/plugins/lsp.lua
@@ -187,7 +187,16 @@ return {
         },
         -- gopls = {},
         basedpyright = {},
-        rust_analyzer = {},
+        rust_analyzer = {
+          settings = {
+            ['rust-analyzer'] = {
+              completion = {
+                addCallArgumentSnippets = false,
+                addCallParenthesis = false,
+              },
+            },
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
